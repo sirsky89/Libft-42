c30157ab4d8359d52d8f0f85569c84976737b5b5
root <root@SkyRulez-PC>
2023-11-10 09:51:20 +0800

    adding ft_lstsize.c

---
+++ ft_lstsize.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	count;
+
+	count = 0;
+	if (lst == NULL)
+		return (0);
+	while (lst)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
+
