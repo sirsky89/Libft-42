ccc681a8776d4db42a127df65b7bbc876ed54637
root <root@SkyRulez-PC>
2023-11-19 11:49:09 +0800

    update ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*p;
+
+	p = s;
+	while (n--)
+	{
+		if (*p == (unsigned char)c)
+			return ((void *)p);
+		p++;
+	}
+	return ((void *)0);
+}
